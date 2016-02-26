2349369332014933fc51e5b77db471db12ef1ecb
Steve Melia <steve.j.melia@gmail.com>
2016-02-26 21:54:28 +0000

    Added local signup

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,20 @@
-module.exports = function(app) {
+module.exports = function(app, passport) {
     app.get('/', function(req, res) {
         res.render('index');
+    });
+
+    app.get('/signup', function(req, res) {
+        res.render('signup', { yo: "Yo!", message: req.flash('signupMessage') });
+    });
+
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/profile',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
+
+    app.get('/login', function(req, res) {
+        res.render('login', { message: req.flash('loginMessage') });
     });
 
     app.get('/profile', function(req, res) {
--- server.js
+++ server.js
@@ -3,13 +3,35 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+// Templating - Mustache
 var mustacheExpress = require('mustache-express');
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
-require('./app/routes.js')(app);
+// Persistence - Mongoose
+var mongoose = require('mongoose');
+var configDb = require('./app/database.js');
+mongoose.connect(configDb.url);
+
+// Auth - Passport
+var passport = require('passport');
+require('./app/passport.js')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Other
+var session = require('express-session');
+app.use(session({ secret: 'thisismysecret' }));
+
+var flash = require('connect-flash');
+app.use(flash());
+
+var bodyParser = require("body-parser");
+app.use(bodyParser());
+
+require('./app/routes.js')(app, passport);
 
 app.listen(port);
 console.log('The magic happens on port ' + port);
