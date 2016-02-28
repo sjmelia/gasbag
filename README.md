1c865cc10a5afbba39b1fc65f12c7a0f8f855974
Steve Melia <steve.j.melia@gmail.com>
2016-02-28 15:26:32 +0000

    Added auth to socket.io chat

---
+++ README.md
@@ -0,0 +1,5 @@
+Mash up of the socket.io chat example http://socket.io/get-started/chat/ with passport.js auth
+
+https://scotch.io/tutorials/easy-node-authentication-setup-and-local
+
+using a little bit of passport.socketio https://www.npmjs.com/package/passport.socketio 
--- server.js
+++ server.js
@@ -18,7 +18,15 @@ mongoose.connect(configDb.url);
 
 // Other
 var session = require('express-session');
-app.use(session({ secret: 'thisismysecret' }));
+var SESSION_SECRET = 'thisismysecret';
+var sessionStore = new session.MemoryStore();
+appSession = session({ 
+    secret: SESSION_SECRET,
+    key: 'express.sid',
+    store: sessionStore
+});
+app.use(appSession);
+console.log("sessionStore: " + sessionStore);
 
 var flash = require('connect-flash');
 app.use(flash());
@@ -41,8 +49,9 @@ io.on('connection', function(socket) {
     console.log("Socket.io connection");
 
     socket.on('new message', function (data) {
+        console.log(socket.request.user);
         io.sockets.emit('new message', {
-            username: socket.username,
+            username: socket.request.user.local.email,
             message: data
         });
     });
@@ -51,6 +60,35 @@ io.on('connection', function(socket) {
         console.log("Socket.io disconnect");
     });
 });
+
+// passport.socketio
+var passportSocketIo = require('passport.socketio');
+io.use(passportSocketIo.authorize({
+    cookieParser: cookieParser,
+    secret: SESSION_SECRET,
+    store: sessionStore,
+    key: 'express.sid',
+    success: onAuthorizeSuccess,
+    fail: onAuthorizeFail,
+}));
+
+function onAuthorizeSuccess(data, accept) {
+    console.log("on auth success");
+    accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+    if (error) {
+        throw new Error(message);
+    }
+    console.log('Failed connection to socket.io:', message);
+
+    accept(null, false);
+
+    if (error) {
+        accept(new Error(message));
+    }
+}
 
 require('./app/routes.js')(app, passport);
 
